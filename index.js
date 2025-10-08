899cf4729fb791892d520cc38b842bab40ddbce5
akashk11 <akashvinod777@gmail.com>
2025-10-08 23:55:50 +0400

    New test api added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World from Docker!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("New test api endpoint");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
